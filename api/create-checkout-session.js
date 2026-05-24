309436153cfa887f2ff1cde79137f2745e539200
removely <info@removely.de>
2026-05-24 22:24:35 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         price_data: {
           currency: 'eur',
           product_data: {
-            name: 'Eye AI – Try & Buy',
+            name: 'ㅤ',
           },
           unit_amount: 4999,
         },
