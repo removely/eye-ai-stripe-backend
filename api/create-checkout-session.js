08782ee27bb5fbcdc6416c6bc04cabbf00cf7367
removely <info@removely.de>
2026-05-25 12:27:38 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // 👇 HIER deine Price-ID der 49,99€-Monatsrate eintragen
-const RESTZAHLUNG_PRICE_ID = 'price_1TaaFDHRgmyzxSIncG8YFKVo';
+const RESTZAHLUNG_PRICE_ID = 'price_1TahnIHRgmyzxSInhDPavGrQ';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
